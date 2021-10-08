beb30c86ee0931241d5bc20f899a18284da26cfd
franklin <2730246+devsecfranklin@users.noreply.github.com>
2021-10-08 16:55:39 -0400

    adding some dev setup tools

--- bin/role.sh
+++ bin/role.sh
@@ -1,8 +1,32 @@
+#/bin/bash
 
-ns="dev-$(whoami)"
+ns="dev-${1}"
+
+if [ "${ns}" == "dev-" ]; then
+  echo "Dont forget the username."
+  exit 0
+fi
+
+echo "Create resource limits for ${1}."
+
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: ResourceQuota
+metadata:
+  name: limit-compute
+  namespace: ${ns}
+spec:
+  hard:
+    requests.cpu: "10"
+    requests.memory: 100Gi
+    limits.cpu: "10"
+    limits.memory: 100Gi 
+EOF
 
 kubectl create namespace ${ns}
 kubectl annotate namespace ${ns} dev=franklin
+
+echo "Create a namespace for ${1}"
 
 cat <<EOF | kubectl create -f -
 apiVersion: rbac.authorization.k8s.io/v1
@@ -16,6 +40,6 @@ roleRef:
   name: edit
 subjects:
 - apiGroup: rbac.authorization.k8s.io
-  kind: user
+  kind: User
   name: $(whoami)
 EOF
--- docs/setup.md
+++ docs/setup.md
@@ -4,5 +4,5 @@
 gcloud container clusters get-credentials secure-dev-lab --zone us-central1
 go run bin/csr-gen.go client <user-name>;
 bin/csr-tool.sh <user-name> client.csr
-kubectl create namespace dev-<user-name>
+bin/role.sh <user-name>
 ```
